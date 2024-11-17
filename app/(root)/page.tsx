432b5bbd07bf08b04bbf305c7f258c135cdd45db
RajkrishnaRana <rajkrishnaranastudy@gmail.com>
2024-11-17 23:59:52 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="center">
+      <h1 className="h1">MyDrive - Storage that is all yours.</h1>
+    </div>
+  );
+}
